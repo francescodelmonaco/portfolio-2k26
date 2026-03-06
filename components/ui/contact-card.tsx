f474b2e5d197e416b450cd1491764d088e300c8f
Francesco Delmonaco <francescodelmonaco1999@gmail.com>
2026-03-06 10:21:04 +0100

    fix contacts link

--- components/ui/contact-card.tsx
+++ components/ui/contact-card.tsx
@@ -1,15 +1,15 @@
-import Link from "next/link"
 import { memo } from "react"
 
 const ContactCard = memo(function ContactCard({ link, children }: { link: string, children: React.ReactNode }) {
     return (
-        <Link
+        <a
             href={link}
             target="_blank"
+            rel="noopener noreferrer"
             className="glass-panel w-full h-full rounded-2xl flex items-center justify-center text-white/70 hover:text-white"
         >
             {children}
-        </Link>
+        </a>
     )
 })
 
